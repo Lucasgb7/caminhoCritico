aa1d6577802d61ec5021d2d65fffa798cd1c898e
Lucas Cunha <lucasgb7_ks@hotmail.com>
2020-06-29 12:02:29 -0300

    backward_pass

--- caminhoCritico.py
+++ caminhoCritico.py
@@ -1,4 +1,3 @@
-import numpy as np
 from Graphs import Graphs
 # https://escritoriodeprojetos.com.br/metodo-do-caminho-critico
 
@@ -9,7 +8,7 @@ def forward_pass(task):
             task[fw]['ES'] = 1
             task[fw]['EF'] = task[fw]['duration']
         else:
-            for k in task.keys():
+            for k in task.keys():***
                 for dependency in task[k]['dependencies']:  # passa por todas as dependencias de uma atividade
                     if dependency != '-1' and len(task[k]['dependencies']) == 1:
                         task[k]['ES'] = int(task[dependency]['EF']) + 1
@@ -20,38 +19,38 @@ def forward_pass(task):
                             task[k]['EF'] = int(task[k]['ES']) + int(task[k]['duration']) - 1
 
 
-def backward_pass(task):
-    for taskBW in bList:
-        if (bList.index(taskBW) == 0):  # check if it's the last task (so no more task)
-            tasks[taskBW]['LF'] = tasks[taskBW]['EF']
-            tasks[taskBW]['LS'] = tasks[taskBW]['ES']
+def backward_pass(task, keys_reverse):
+    for bw in keys_reverse:
+        if (keys_reverse.index(bw) == 0):   # verifica se e a ultima atividade
+            tasks[bw]['LF'] = tasks[bw]['EF']
+            tasks[bw]['LS'] = tasks[bw]['ES']
 
-        for dipendenza in tasks[taskBW]['dependencies']:  # slides all the dependency in a single task
-            if (dipendenza != '-1'):  # check if it's NOT the last task
-                if (tasks['task' + dipendenza]['LF'] == 0):  # check if the the dependency is already analyzed
-                    # print('ID dipendenza: '+str(tasks['task'+dipendenza]['id']) + ' taskBW: '+str(tasks[taskBW]['id']))
-                    tasks['task' + dipendenza]['LF'] = int(tasks[taskBW]['LS']) - 1
-                    tasks['task' + dipendenza]['LS'] = int(tasks['task' + dipendenza]['LF']) - int(
-                        tasks['task' + dipendenza]['duration']) + 1
-                    tasks['task' + dipendenza]['float'] = int(tasks['task' + dipendenza]['LF']) - int(
-                        tasks['task' + dipendenza]['EF'])
-                    # print('IF1 dip LS: '+str(tasks['task'+dipendenza]['LS']) +' dip LF: '+str(tasks['task'+dipendenza]['LF']) + ' taskBW: '+str(tasks[taskBW]['id'])+' taskBW ES '+ str(tasks[taskBW]['ES']))
-                if (int(tasks['task' + dipendenza]['LF']) > int(
-                        tasks[taskBW]['LS'])):  # put the minimun value of LF for the dependencies of a task
-                    tasks['task' + dipendenza]['LF'] = int(tasks[taskBW]['LS']) - 1
-                    tasks['task' + dipendenza]['LS'] = int(tasks['task' + dipendenza]['LF']) - int(
-                        tasks['task' + dipendenza]['duration']) + 1
-                    tasks['task' + dipendenza]['float'] = int(tasks['task' + dipendenza]['LF']) - int(
-                        tasks['task' + dipendenza]['EF'])
-                    # print('IF2 dip LS: '+str(tasks['task'+dipendenza]['LS']) +' dip LF: '+str(tasks['task'+dipendenza]['LF']) + ' taskBW: '+str(tasks[taskBW]['id']))
+        for dependency in tasks[bw]['dependencies']:  # slides all the dependency in a single task
+            if (dependency != '-1'):  # check if it's NOT the last task
+                if (tasks[dependency]['LF'] == 0):  # check if the the dependency is already analyzed
+                    # print('ID dependency: '+str(tasks['task'+dependency]['id']) + ' bw: '+str(tasks[bw]['id']))
+                    tasks[dependency]['LF'] = int(tasks[bw]['LS']) - 1
+                    tasks[dependency]['LS'] = int(tasks[dependency]['LF']) - int(
+                        tasks[dependency]['duration']) + 1
+                    tasks[dependency]['float'] = int(tasks[dependency]['LF']) - int(
+                        tasks[dependency]['EF'])
+                    # print('IF1 dip LS: '+str(tasks['task'+dependency]['LS']) +' dip LF: '+str(tasks['task'+dependency]['LF']) + ' bw: '+str(tasks[bw]['id'])+' bw ES '+ str(tasks[bw]['ES']))
+                if (int(tasks[dependency]['LF']) > int(
+                        tasks[bw]['LS'])):  # put the minimun value of LF for the dependencies of a task
+                    tasks[dependency]['LF'] = int(tasks[bw]['LS']) - 1
+                    tasks[dependency]['LS'] = int(tasks[dependency]['LF']) - int(
+                        tasks[dependency]['duration']) + 1
+                    tasks[dependency]['float'] = int(tasks[dependency]['LF']) - int(
+                        tasks[dependency]['EF'])
+                    # print('IF2 dip LS: '+str(tasks['task'+dipendenza]['LS']) +' dip LF: '+str(tasks['task'+dipendenza]['LF']) + ' bw: '+str(tasks[bw]['id']))
 
 if __name__ == '__main__':
-    # Tabela de atividades
+    # Tabela de  atividades
     # activity = ['A', 'B', 'C', 'D', 'E', 'F']    # atividades
     # duration = [10, 4, 7, 5, 5, 2]               # duracao de cada atividade
     # previous = ['', 'A', 'A', 'C', 'B,D', 'C']  # precedente das atividades
     activity = ['0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '1']  # atividades
-    duration = [0, 6, 2, 3, 10, 3, 2, 4, 5, 8, 6, 4, 2]  # duracao de cada atividade
+    duration = [0, 6, 2, 3, 10, 3, 2, 4, 5, 8, 6, 4, 2, 0]  # duracao de cada atividade
     previous = ['', '0', '0', '0', 'A', 'A', 'B', 'C', 'E', 'F,G', 'G', 'I', 'J',
                 'D,H,K,L']  # precedente das atividades
     table_length = len(activity)
@@ -75,12 +74,22 @@ if __name__ == '__main__':
         task[i]['float'] = 0
         task[i]['critical'] = False
 
-    forward_pass(task)
+    forward_pass(task) # faz o caminho de ida
+
+    keys = list()
+    for e in task.keys(): # lista das chaves das atividades
+        keys.append(e)
+
+    keys_reverse = list()
+    while len(keys) > 0:    # lista das chaves ao contrario para caminho de volta
+        keys_reverse.append(keys.pop())
+    
+    backward_pass(task, keys_reverse)
+
     edges = []
     for i in range(table_length):  # Em cada atividade
         if len(previous[i]) < 1:
             root = activity[i]  # vertice inicial
-            task['id'] = root
             edges.append([root, root])  # caso nao tenha antecessor e o proprio vertice inicial
         elif len(previous[i]) > 1:
             aux = previous[i].split(',')
@@ -90,10 +99,9 @@ if __name__ == '__main__':
             edges.append([previous[i], activity[i]])  # Adiciona o antecessor
 
     print("Arestas: ", edges)  # add edges
-    graph = Graphs(edges, True)  # insere as arestas, e determina como 'direcionado'
-    visited = []
-    dfs(graph, root, visited)
-    print("DFS: ", visited)
-    visited = []
-    bfs(graph, root, visited)
-    print("BFS: ", visited)
+
+    print('task id, task name, duration, ES, EF, LS, LF, float, isCritical')
+    for task in tasks:
+        if(tasks[task]['float'] == 0): # folga = 0 faz parte do caminho critico
+            tasks[task]['isCritical'] = True
+        print(str(tasks[task]['id']) +', '+str(tasks[task]['name']) +', '+str(tasks[task]['duration']) +', '+str(tasks[task]['ES']) +', '+str(tasks[task]['EF']) +', '+str(tasks[task]['LS']) +', '+str(tasks[task]['LF']) +', '+str(tasks[task]['float']) +', '+str(tasks[task]['isCritical']))
